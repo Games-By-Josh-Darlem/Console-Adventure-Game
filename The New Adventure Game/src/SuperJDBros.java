624f75fbf60444b73fb40f1d1ee38d5baea378b1
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-01-27 21:38:45 -0600

    Super JD Bros part 3.

--- The New Adventure Game/src/SuperJDBros.java
+++ The New Adventure Game/src/SuperJDBros.java
@@ -1,10 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class SuperJDBros {
 
     public static void main(String[] args) {
         choosePlayer();
-
+        levelOne(0, 10);
     }
 
     public static void choosePlayer() {
@@ -27,9 +28,38 @@ public class SuperJDBros {
                 System.out.println("By pressing B, you have chosen " + playerB + "!");
             }
         }
-
-//        public static void theFight() {
-//
-//        }
     }
+    public static void levelOne(int min, int max) {
+        Random randLvl = new Random();
+        int upperbound = 10;
+
+
+        String lvlTwo = "level 2";
+        int int_random = randLvl.nextInt(upperbound);
+        for (int i = 0; i <= max; i++) {
+
+        }
+
+
+        System.out.println("The goomba's strength, that was defeated, level " + int_random + "," + " need to defeat more enemies");
+
+        System.out.println("The turtle's strength, that was defeated, was level " + int_random +"," + " you have defeated enough enemies to move to " + lvlTwo);
+    }
+//    public static long getPowerRecursive(int base, int exponent) { // 1. getPowerRecursive(6, 3)  -- goal: 6 * 6 * 6
+//        if (exponent == 0) {
+//            return 1; // any number to the power of 0 is equal to 1
+//        } else if (exponent == 1) {
+//            return base; // any number to the power of 1 is equal to itself
+//        } else if (exponent == 2) {
+//            return base * base;
+//        }
+//        return base * getPowerRecursive(base, exponent - 1);  // we want to do 3 ^ 5, i.e. getPowerRecursive(3, 5)
+//
+//        // 1. return 3 * getPowerRecursive(3, (5-1)), i.e. getPowerRecursive(3, 4)
+//        // 2. return 3 * ( 3 * getPowerRecursive(3, (4-1)) ), i.e. getPowerRecursive(3, 3)
+//        // 3. return 3 * ( 3 * ( 3 * ( getPowerRecursive(3, (3-1)) ) , i.e. getPowerRecursive(3, 2)
+//        //  3.a) return 3 * ( 3 * ( 3 ) ) ) * 3 * 3;
+//        ///  = 3 * 3 * 3 * 3 * 3
+//        ///  = 3 ^ 5 = 243
+//    }
 }
